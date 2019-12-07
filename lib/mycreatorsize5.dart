b7b49a37fc1e99974f04c58c9e1a25dd232be737
paulbryche <47352240+paulbryche@users.noreply.github.com>
2019-12-07 17:35:55 +0100

    pdf generated but need to print it now
    TODO LIST:
    -generate pdf
    -pdf viewers to integrate
    -chose where to put the pdf file with user access

--- lib/mycreatorsize5.dart
+++ lib/mycreatorsize5.dart
@@ -22,6 +22,64 @@ class MyCreatorSize5 extends StatefulWidget {
 }
 
 class MyCreatorSize5State extends State<MyCreatorSize5> {
+  String generatedPdfFilePath;
+
+  @override
+  void initState() {
+    super.initState();
+    generateExampleDocument();
+  }
+
+  Future<void> generateExampleDocument() async {
+    var htmlContent = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <style>
+        table, th, td {
+          border: 1px solid black;
+          border-collapse: collapse;
+        }
+        th, td, p {
+          padding: 5px;
+          text-align: left;
+        }
+        </style>
+      </head>
+      <body>
+        <h2>PDF Generated with flutter_html_to_pdf plugin</h2>
+
+        <table style="width:100%">
+          <caption>Sample HTML Table</caption>
+          <tr>
+            <th>Month</th>
+            <th>Savings</th>
+          </tr>
+          <tr>
+            <td>January</td>
+            <td>100</td>
+          </tr>
+          <tr>
+            <td>February</td>
+            <td>50</td>
+          </tr>
+        </table>
+
+        <p>Image loaded from web</p>
+        <img src="https://i.imgur.com/wxaJsXF.png" alt="web-img">
+      </body>
+    </html>
+    """;
+
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    var targetPath = appDocDir.path;
+    var targetFileName = "example-pdf";
+
+    var generatedPdfFile = await FlutterHtmlToPdf.convertFromHtmlContent(
+        htmlContent, targetPath, targetFileName);
+    generatedPdfFilePath = generatedPdfFile.path;
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
