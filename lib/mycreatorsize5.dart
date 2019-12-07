09528906fa3f87454f4d45e3dccb7d17c5cd2481
paulbryche <47352240+paulbryche@users.noreply.github.com>
2019-12-07 15:35:36 +0100

    page to generate pdf done now need to generate pdf and print it (packages added)
    TODO LIST:
    -generate pdf
    -pdf viewers to integrate
    -chose where to put the pdf file with user access

--- lib/mycreatorsize5.dart
+++ lib/mycreatorsize5.dart
@@ -1,4 +1,8 @@
 import 'dart:io';
+import 'dart:async';
+
+import 'package:flutter_html_to_pdf/flutter_html_to_pdf.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
